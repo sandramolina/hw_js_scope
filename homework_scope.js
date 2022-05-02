de6834bf85eec4a30b5bbeef112422e0de5b8276
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-02 17:57:01 +0100

    adds episode 4

--- homework_scope.js
+++ homework_scope.js
@@ -41,3 +41,17 @@
 
 // const secondVerdict = `The murderer is ${murderer}.`;
 // console.log("Second Verdict: ", secondVerdict);
+
+//Episode 4
+// let suspectOne = "Miss Scarlet";
+// let suspectTwo = "Professor Plum";
+// let suspectThree = "Mrs. Peacock";
+
+// const declareAllSuspects = function () {
+//   let suspectThree = "Colonel Mustard";
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// };
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
