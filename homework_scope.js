a567b31089158e4d5203c588883f596220127f80
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-02 17:54:26 +0100

    adds episode 3

--- homework_scope.js
+++ homework_scope.js
@@ -14,16 +14,30 @@
 
 //Episode 2
 
-const murderer = "Professor Plum";
+// const murderer = "Professor Plum";
 
-const changeMurderer = function () {
-  let murderer = "Mrs. Peacock";
-};
+// const changeMurderer = function () {
+//   let murderer = "Mrs. Peacock";
+// };
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// };
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(`Episode 2 veridict: ${verdict}`);
+
+//Episode 3
+let murderer = "Professor Plum";
 
 const declareMurderer = function () {
+  let murderer = "Mrs. Peacock";
   return `The murderer is ${murderer}.`;
 };
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(`Episode 2 veridict: ${verdict}`);
+const firstVerdict = declareMurderer();
+console.log("First Verdict: ", firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log("Second Verdict: ", secondVerdict);
