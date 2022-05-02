6463331329842c31aabbffef698e58ff7593014a
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-02 18:06:49 +0100

    adds episode 6

--- homework_scope.js
+++ homework_scope.js
@@ -74,3 +74,24 @@
 // changeWeapon("Revolver");
 // const verdict = declareWeapon();
 // console.log(verdict);
+
+//Episode 6
+let murderer = "Colonel Mustard";
+
+const changeMurderer = function () {
+  murderer = "Mr. Green";
+
+  const plotTwist = function () {
+    murderer = "Mrs. White";
+  };
+
+  plotTwist();
+};
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+};
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
