870fee262701042ab9dc4bceeb045163ea447088
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-05-02 20:21:22 +0100

    adds episode 8

--- homework_scope.js
+++ homework_scope.js
@@ -122,3 +122,38 @@
 // changeMurderer();
 // const verdict = declareMurderer();
 // console.log(verdict);
+// Episode 8
+const scenario = {
+  murderer: "Mrs. Peacock",
+  room: "Conservatory",
+  weapon: "Lead Pipe",
+};
+
+const changeScenario = function () {
+  scenario.murderer = "Mrs. Peacock";
+  scenario.room = "Dining Room";
+
+  const plotTwist = function (room) {
+    if (scenario.room === room) {
+      scenario.murderer = "Colonel Mustard";
+    }
+
+    const unexpectedOutcome = function (murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = "Candle Stick";
+      }
+    };
+
+    unexpectedOutcome("Colonel Mustard");
+  };
+
+  plotTwist("Dining Room");
+};
+
+const declareWeapon = function () {
+  return `The weapon is ${scenario.weapon}.`;
+};
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
